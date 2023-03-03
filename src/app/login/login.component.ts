55bcd4c8f09a3db60989510ec12ac751b3d85857
Leandro Mak Yu <leandro.yu@ibm.com>
2023-03-03 10:53:27 -0300

    update login screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,27 +10,32 @@ import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   faBar = faBars;
   faBell = faBell;
   faSortDown = faSortDown;
   account: string = '';
-  password: string = '';
+  agencia: string = '';
+  conta: string = '';
 
-  constructor(private http: HttpClient, private router: Router, private dialog: MatDialog) {}
+  constructor(private http: HttpClient, private router: Router, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.account = 'Malu Carvalho';
+    this.agencia = '1234';
+    this.conta = '12345-6';
   }
 
   clearInput() {
     this.account = '';
+    this.agencia = '';
+    this.conta = '';
   }
 
   onSubmit() {
-    const loginData = {account: this.account};
+    const loginData = { account: this.account, agencia: this.agencia, conta: this.conta };
     this.http.post('api/v1/login', loginData).subscribe(
-      response => console.log('Sucess!', response),
+      response => this.router.navigate(['/cadastro']),
       error => this.dialog.open(ErrorDialogComponent)
     );
   }
